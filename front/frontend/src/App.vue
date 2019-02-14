8e767dd7d57e7548c6c16e54786430732a6df0f0
s17001 <s17001@std.it-college.ac.jp>
2019-02-14 19:03:23 +0900

    frontの統合環境用にvuecliをinitして下処理した

---
+++ front/frontend/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div>
+      <ul>
+        <li>
+          <a href="#/"><img style="max-width: 50px" src="./assets/logo.png"></a>
+          <a href="#/register">登録</a>
+          <a href="#/show">閲覧</a>
+        </li>
+      </ul>
+    </div>
+
+    <router-view/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+  li{
+    list-style: none;
+  }
+  ul{
+    list-style: none;
+  }
+</style>
---
+++ front/frontend/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/register'
+import Show from '@/components/show'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/show',
+      name: 'Show',
+      component: Show
+    }
+  ]
+})
